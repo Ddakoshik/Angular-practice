36a83c761c3c475fcfc23a758f2f4b89269f2ac9
Andru Ischenko <roobotik@gmail.com>
2018-02-20 14:29:55 +0200

    separation on component

--- src/app/tasks/tasks.component.ts
+++ src/app/tasks/tasks.component.ts
@@ -1,12 +1,14 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { Hero } from '../hero';
 
 @Component({
   selector: 'app-tasks',
   templateUrl: './tasks.component.html',
   styleUrls: ['./tasks.component.css']
 })
-export class TasksComponent implements OnInit {
 
+export class TasksComponent implements OnInit {
+  @Input() hero: Hero;
 
   constructor() { }
 
