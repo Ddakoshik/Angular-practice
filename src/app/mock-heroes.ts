ce8da1e9b653d909ced4e600f62b0f12be2b5fd5
Andru <roobotik@gmail.com>
2018-03-09 11:00:32 +0200

    1

--- src/app/mock-heroes.ts
+++ src/app/mock-heroes.ts
@@ -1,14 +1,14 @@
 import { Hero } from './hero';
 
 export const HEROES: Hero[] = [
-  { id: 11, name: 'Mr. Nice', status: 'start' },
-  { id: 12, name: 'Narco' , status: 'start' },
-  { id: 13, name: 'Bombasto' , status: 'start' },
-  { id: 14, name: 'Celeritas' , status: 'start'},
-  { id: 15, name: 'Magneta' , status: 'start' },
-  { id: 16, name: 'RubberMan' , status: 'start' },
-  { id: 17, name: 'Dynama' , status: 'start' },
-  { id: 18, name: 'Dr IQ' , status: 'start'},
-  { id: 19, name: 'Magma' , status: 'start'},
-  { id: 20, name: 'Tornado' , status: 'start'}
+  { id: 11, name: 'Mr. Nice' },
+  { id: 12, name: 'Narco'  },
+  { id: 13, name: 'Bombasto'  },
+  { id: 14, name: 'Celeritas' },
+  { id: 15, name: 'Magneta'  },
+  { id: 16, name: 'RubberMan'  },
+  { id: 17, name: 'Dynama'  },
+  { id: 18, name: 'Dr IQ' },
+  { id: 19, name: 'Magma' },
+  { id: 20, name: 'Tornado' }
 ];
